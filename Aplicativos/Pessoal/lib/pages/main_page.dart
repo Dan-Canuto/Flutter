238a2034ce1c1e9c83929642a0c159c83916641c
DanDanCa <danilodcp18@gmail.com>
2023-12-12 20:40:01 -0300

    :sparkles: feat: Iniciando projeto

---
+++ Aplicativos/Pessoal/lib/pages/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Organizador"),
+        
+      ),
+      body: Placeholder(),
+      
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
