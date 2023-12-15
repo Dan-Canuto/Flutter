ef9a28bfd09a2f4bdee6c0c8d021b9e7e81a84d8
DanDanCa <danilodcp18@gmail.com>
2023-12-15 00:10:41 -0300

    :sparkles: feat: Inicio da Tela de Entrada

--- Aplicativos/Pessoal/lib/main.dart
+++ Aplicativos/Pessoal/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromRGBO(150, 150, 150, 0.1),
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
--- Aplicativos/Pessoal/lib/pages/main_page.dart
+++ Aplicativos/Pessoal/lib/pages/main_page.dart
@@ -10,13 +10,66 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Organizador"),
-        
-      ),
-      body: Placeholder(),
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('MY\nCOMPANION',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Color.fromRGBO(17, 217, 124, 0.7),
+            ),),
+            SizedBox(
+              height: mediaQuery.size.height * 0.15,
+            ),
+            Container(
+              padding: EdgeInsets.all(12),
+              width: mediaQuery.size.width * 0.8,
+              height: mediaQuery.size.height * 0.3,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(17, 217, 124, 0.7),
+                border: Border.all(
+                  style: BorderStyle.solid,
+                  width: 5,
+                  color: Color.fromRGBO(13, 166, 95, 0.8),
+                ),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "SENHA",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromRGBO(4, 38, 22, 1),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 16),
+                    color: Colors.white,
+                    height: 30,
+                    width: 400,
+                  ),
+                  ElevatedButton(
+                    onPressed: (){},
+                    style: ButtonStyle(
+                      backgroundColor: Color.fromRGBO(4, 38, 22, 1),
+                    ),
+                    child: Text("Enviar"))
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+
       
       // This trailing comma makes auto-formatting nicer for build methods.
     );
